6818b98548a917dac80d7beea9b691b9efaf4b8f
Vedant Kadam <kvedant451@gmail.com>
2025-04-13 02:15:00 +0530

    Updated SQLBasic File

--- SQL_Basics.sql
+++ SQL_Basics.sql
@@ -9,3 +9,21 @@ payment_method varchar(20)
 
 insert into amazon_orders(1, '2025-04-13', 'Earphones', 30.5, 'UPI');
 insert into amazon_orders(2, '2025-04-11', 'Iphone', 500, 'Credit Card');
+
+-- limiting columns or selecting specific columns
+select order_id, product from amazon_orders;
+--limiting rows
+select top 5 * from amazon_orders		-- wil give top 5 rows only 
+
+/*
+data types in sql
+integer -> 1,2,3, -1, -2 
+date -> "2025-04-13"
+varchar(100) -> "earphones"
+decimal(6, 2) -> 1234.45  	==> here 6 is the length and 2 are numbers after decimal
+
+drive -- folder1 
+drive -- folder 2
+	
+
+*/
